bd3c26664e60256aca69a75a932908bc8c166f53
Steve Carter <steve.carter@etas.com>
2022-12-07 12:16:22 +0000

    put instructions for step 1 into the calculate function

--- workshop.py
+++ workshop.py
@@ -1,5 +1,13 @@
 
 """
+"""
+
+import doctest
+
+#def
+
+def calculate( operator_name, operand_1, operand_2):
+    """ 
     Write a basic Python "calculator".
     It should accept 3 pieces of input from the user: a string that's one of "x", "+", "
     "--", or
@@ -8,20 +16,20 @@
 
     For example, if your application asks the user for an operation and 2 numbers, and
     the user enters "+", "1", "2", then the application should output "3".
-    If the user supplied "/", "5", "2", the application should output "2.5".
-    If the user supplied "x", "5", "0", the application should output 0.
-"""
 
-import doctest
-
-def calculate( operator_name, operand_1, operand_2):
-    """ Calculator for corndel exercise step 1
     >>> calculate( '+', 1, 2)
     3
+
+    If the user supplied "/", "5", "2", the application should output "2.5".
+
     >>> calculate( '/', 5, 2)
     2.5
+
+    If the user supplied "x", "5", "0", the application should output 0.
+
     >>> calculate( 'x', 5, 0)
     0
+
     """
     if operator_name == '+':
         return operand_1 + operand_2
